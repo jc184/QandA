d81b6490aa07f5181480b43fb6c8112a0e6a6998
james chalmers <james.chalmers184@gmail.com>
2020-06-19 20:28:55 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
